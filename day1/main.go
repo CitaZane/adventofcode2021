8b7dfe45480c269c0c95d3ab836eca877fa449c2
CitaZane <79696183+CitaZane@users.noreply.github.com>
2021-12-01 08:58:37 +0200

    Add files via upload

---
+++ day1/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	nums := readInts("data.txt")
+	firstCount := firstMesurement(nums)
+	fmt.Println("First Scan: ", firstCount)
+	secondCount := secondMesurement(nums)
+	fmt.Println("Second Scan: ", secondCount)
+}
+
+func firstMesurement(data []int) int {
+	count := 0
+	for i := 0; i < len(data)-1; i++ {
+		if data[i+1] > data[i] {
+			count++
+		}
+	}
+	return count
+}
+
+// 1752
+
+func secondMesurement(data []int) int {
+	count := 0
+	for i := 0; i < len(data)-3; i++ {
+		one := data[i]
+		two := data[i+1]
+		three := data[i+2]
+		four := data[i+3]
+		if one+two+three < two+three+four {
+			count++
+		}
+	}
+	return count
+}
+
+// 1781
+
+func readInts(file string) []int {
+	d, _ := ioutil.ReadFile(file)
+	lines := strings.Split(string(d), "\n")
+	nums := make([]int, 0, len(lines))
+	for _, l := range lines {
+		if len(l) == 0 {
+			continue
+		}
+		n, _ := strconv.Atoi(l)
+		nums = append(nums, n)
+	}
+	return nums
+}
